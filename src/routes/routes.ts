06f61ec43a804b4b62e7f8d03fd33752caa6c234
LuWroblewski <wrolucas@gmail.com>
2025-04-24 22:57:38 -0300

    change name route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,14 @@
 import { Router, type Request, type Response } from 'express';
-import cultivationRoutes from './cultivation/cultivation';
+import cultivationRoutes from './crop/crop';
 import mainRoute from './main/main';
 
 const router = Router();
 
 router.use('/', mainRoute);
-router.use('/cultivation', cultivationRoutes);
+router.use('/crop', cultivationRoutes);
 
 router.use((req: Request, res: Response) => {
-  res.status(404).json({ message: 'Not Found' });
+  res.redirect('/404');
 });
 
 export default router;
